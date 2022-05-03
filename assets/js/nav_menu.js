78c87b5b575e991c3ed6b05fca62f7d445ed7cc7
Kevin Can Duzguner <67980369+kevinbaddata@users.noreply.github.com>
2022-05-03 09:20:07 +0200

    Commited

---
+++ assets/js/nav_menu.js
@@ -0,0 +1,16 @@
+// Execute code snippet below on click event
+$('.wrapper').on('click', function() {
+
+    // Check if span doesn't contain grill class
+    if (!$('span').hasClass("grill")) {
+        $('span').addClass('grill');
+        $('h1').show();
+        $('.slider').addClass('showslide');
+    }
+    // Remove class grill if class doesn't contain
+    else {
+        $('span').removeClass('grill');
+        $('h1').hide();
+        $('.slider').removeClass('showslide');
+    }
+});
